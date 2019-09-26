696f8c24d98220f23645bde40525559e83ebc5d0
Scuall1992 <scuall1992@gmail.com>
2019-09-26 21:12:22 +0300

    Почистили от мусора и попытались приукрасить

--- main.py
+++ main.py
@@ -28,10 +28,30 @@ def extract_text_from_pdf(pdf_path):
         return text
 
 
+def replace_smht(s, what):
+    for i in what:
+        s = s.replace(i, '', 1)
+    return s
+
+
 if __name__ == '__main__':
     data = extract_text_from_pdf('2.pdf')
 
-    res = [(m.start(0), m.end(0)) for m in re.finditer(r"[0-9]{1,2}\.( )*[0-9]{1,2}\.( )*[0-9]{1,2}", data)]
+    res = [(m.start(0), m.end(0)) for m in re.finditer(r" [0-9]{1,2}\.( )*[0-9]{1,2}\.( )*[0-9]{1,2}", data)]
+    parsed = []
     with open("res.csv", "w", encoding="utf-8") as f:
         for i in range(len(res) - 1):
-            f.write(data[res[i][1]:res[i + 1][1]].replace(". ", "", 1) + "\n")
+            parsed.append(data[res[i][1]:res[i + 1][1]].replace(". ", "", 1))
+
+        buf = []
+        for i in range(len(parsed) - 1):
+            if not parsed[i][0].isalpha() and parsed[i][0] != "'":
+                buf.append(parsed[i])
+            else:
+                buf[-1] += f" {parsed[i]}"
+
+        what = ["' ", ", ", "^ ", " ", "Patemanmeldungen.",
+                "Patemanmeldungen. ", ".", "»", "!",
+                "Patentllnme ldunge n ", "Patentanmeldungen"
+                ]
+        f.write("\n".join([i if i[0].isdigit() else replace_smht(i, what) for i in buf]))
